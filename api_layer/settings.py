a135b66859ebc0de169a09dad2ede9dec0ebcbf2
Vishesh Bangotra <visheshbangotra@gmail.com>
2017-03-03 17:35:42 +0530

    added service app

--- api_layer/settings.py
+++ api_layer/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'aetos_material',
+    'services',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ services/models.py
@@ -0,0 +1,16 @@
+from api_layer.custom_model_class import AetosModel
+
+# Django Imports
+from django.db import models
+
+
+class Service(AetosModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+
+    def __str__(self):
+        return self.name
