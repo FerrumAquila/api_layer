8b5c78afaa21eaa635fe4d4e42e5c9e45f4824be
Vishesh Bangotra <visheshbangotra@gmail.com>
2017-03-05 20:16:14 +0530

    added dump* in gitignore and changed views to take hardcoded service api to serialiser and return data

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,6 @@
+# App Imports
+from services import models as service_models
+
 # Django Imports
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -5,5 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def test_api(request, api_version):
-    response = JsonResponse(data={'success': True, 'api_version': api_version})
+    data = service_models.ServiceAPI.objects.all().latest('id').serialiser(
+        {'data': {'success': True, 'api_version': api_version}}).required_json
+    response = JsonResponse(data=data)
     return response
