b41ebee9acd12579ae1e4756f1e08da74ef6c650
Vishesh Bangotra <visheshbangotra@gmail.com>
2017-03-11 23:50:36 +0530

    added request and response body maps and added fresh fixtures and removed old ones from git

--- apis/models.py
+++ apis/models.py
@@ -12,7 +12,8 @@ from django.db import models
 
 
 class EndPoint(AetosModel):
-    body_map = models.TextField(default='{}')
+    request_map = models.TextField(default='{}')
+    response_map = models.TextField(default='{}')
     name = models.CharField(max_length=255)
     service_apis = models.ManyToManyField(service_models.ServiceAPI, related_name='endpoints')
 
@@ -23,14 +24,26 @@ class EndPoint(AetosModel):
     def fetch_data(self, params_data):
         api_data = []
         for service_api in self.service_apis.all():
-            instance = {'root_data': service_api.fetch_data(params_data)}
-            api_data.append(self.serialiser(instance).required_json)
+            api_params = self.request_serialiser({'root_data': params_data}).required_json
+            api_response = {'root_data': service_api.fetch_data(api_params['d'])}
+            api_data.append(self.response_serialiser(api_response).required_json)
         return api_data if len(api_data) > 1 else api_data[0]
 
     @property
-    def serialiser(self, versioned=False):
-        body_map = {required_key: self._parse_key_info(key_info)
-                    for required_key, key_info in json.loads(self.body_map).items()}
+    def request_serialiser(self, versioned=False):
+        return self._serialiser('request', versioned)
+
+    @property
+    def response_serialiser(self, versioned=False):
+        return self._serialiser('response', versioned)
+
+    def _serialiser(self, sr_type, versioned=False):
+        map_dict = getattr(self, '%s_map' % sr_type) if sr_type in ['request', 'response'] else None
+        if map_dict:
+            body_map = {required_key: self._parse_key_info(key_info)
+                        for required_key, key_info in json.loads(map_dict).items()}
+        else:
+            raise Exception('Only Request and Response Body Maps Available')
 
         class APISerialiser(Serializer):
             BODY_MAP = body_map
